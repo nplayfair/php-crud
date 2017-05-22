f8d63aab45907368099264cff0a2372941294ec0
Nick Playfair <nick@chaosemerald.co.uk>
2017-05-22 14:51:37 +0100

    Display users from database or error message

--- public/read.php
+++ public/read.php
@@ -1,4 +1,72 @@
-<?php include "templates/header.php"; ?>
+<?php
+if (isset($_POST['submit'])) {
+
+	try {
+		require "../config.php";
+		require "../common.php";
+
+		$connection = new PDO($dsn, $username, $password, $options);
+		// fetch users
+		$sql = "SELECT *
+						FROM users
+						WHERE location = :location";
+		$location = $_POST['location'];
+
+		$statement = $connection->prepare($sql);
+		$statement->bindParam(':location', $location, PDO::PARAM_STR);
+		$statement->execute();
+
+		$result = $statement->fetchAll();
+	}
+	catch(PDOException $error) {
+		echo $sql . "<br>" . $error->getMessage();
+	}
+}
+?>
+
+<?php require "templates/header.php"; ?>
+
+<?php
+if (isset($_POST['submit'])) {
+	if ($result && $statement->rowCount() > 0) {
+		//open table
+		?>
+			<h2>Results</h2>
+			<table>
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Email Address</th>
+						<th>Age</th>
+						<th>Location</th>
+						<th>Date</th>
+					</tr>
+				</thead>
+				<tbody>
+				<?php foreach ($result as $row) { ?>
+				<tr>
+					<td><?php echo escape($row["id"]); ?></td>
+					<td><?php echo escape($row["firstname"]); ?></td>
+					<td><?php echo escape($row["lastname"]); ?></td>
+					<td><?php echo escape($row["email"]); ?></td>
+					<td><?php echo escape($row["age"]); ?></td>
+					<td><?php echo escape($row["location"]); ?></td>
+					<td><?php echo escape($row["date"]); ?></td>
+				</tr>
+				<?php } ?>
+				</tbody>
+			</table>
+			<?php
+		}
+		else
+			{ ?>
+			<blockquote>No results found for <?php echo escape($_POST['location']);
+			?>.</blockquote>
+				<?php
+			}
+		} ?>
 
 <h2>Find a user based on location</h2>
 
@@ -10,4 +78,4 @@
 
 <a href="index.php">Back Home</a>
 
-<?php include "templates/footer.php"; ?>
+<?php require "templates/footer.php"; ?>
