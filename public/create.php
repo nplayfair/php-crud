319b00c93642c72e3c3cdf1dfa083ce53c2a8b41
Nick Playfair <nick@chaosemerald.co.uk>
2017-05-22 13:47:58 +0100

    Add user code

--- public/create.php
+++ public/create.php
@@ -1,4 +1,37 @@
-<?php include "templates/header.php"; ?>
+<?php 
+	include "templates/header.php";
+
+	if (isset($_POST['submit'])) {
+		require "../config.php";
+
+		try {
+			$connection = new PDO($dsn, $username, $password, $options);
+			// insert new user
+			$new_user = array(
+				"firstname" => $_POST['firstname'],
+				"lastname" 	=> $_POST['lastname'],
+				"email" 		=> $_POST['email'],
+				"age" 			=> $_POST['age'],
+				"location" 	=> $_POST['location']
+				);
+
+			$sql = sprintf(
+				"INSERT INTO %s (%s) values (%s)",
+				"users",
+				implode(", ", array_keys($new_user)),
+				":" . implode(", :", array_keys($new_user))
+				);
+
+			$statement = $connection->prepare($sql);
+			$statement->execute($new_user);
+
+		}
+		catch(PDOException $error) {
+			echo $sql . "<br>" . $error->getMessage();
+		}
+
+	}
+?>
 
 	<h2>Add a User</h2>
 
